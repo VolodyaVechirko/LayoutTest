0a81c9d01830b897df7df1570db02a127b99c418
vvechirko <volodya.vechirko@gmail.com>
2019-12-13 16:22:55 +0200

    - set view to adapter

--- app/src/main/java/com/vvechirko/layouttest/ColumnsListView.kt
+++ app/src/main/java/com/vvechirko/layouttest/ColumnsListView.kt
@@ -9,8 +9,7 @@ import android.widget.LinearLayout
 
 class ColumnsListView(context: Context, attrs: AttributeSet?) : LinearLayout(context, attrs) {
 
-    private var spanCount = 3
-    private var columns: List<LinearLayout> = listOf()
+    val spanCount = 3
     private var adapter: Adapter<*>? = null
 
     init {
@@ -27,39 +26,30 @@ class ColumnsListView(context: Context, attrs: AttributeSet?) : LinearLayout(con
             childList.add(v)
             addView(v, LayoutParams(0, LayoutParams.WRAP_CONTENT, 1.0f))
         }
-        columns = childList
     }
 
     fun setAdapter(adapter: Adapter<*>) {
-        this.adapter = adapter.apply {
-            init(columns)
+        this.adapter = adapter.also {
+            it.init(this)
         }
     }
 
     abstract class Holder<T>(val view: View) {
-
-        var column: Int = 0
-        var position: Int = 0
-
         abstract fun bind(item: T)
     }
 
     abstract class Adapter<T> {
-        private var spanCount = 0
-        private var columns: List<LinearLayout> = listOf()
+        lateinit var listView: ColumnsListView
         private var columnItems: List<MutableList<T>> = listOf()
-
-//        private var holders: List<Holder<T>> = listOf()
 
         abstract fun onCreateHolder(parent: ViewGroup): Holder<T>
         abstract fun onBindHolder(holder: Holder<T>, item: T)
 
-        fun init(columns: List<LinearLayout>) {
-            this.spanCount = columns.size
-            this.columns = columns
+        fun init(parent: ColumnsListView) {
+            this.listView = parent
 
             this.columnItems = mutableListOf<MutableList<T>>().apply {
-                for (i in 0 until spanCount) {
+                for (i in 0 until listView.spanCount) {
                     add(mutableListOf())
                 }
             }
@@ -70,7 +60,7 @@ class ColumnsListView(context: Context, attrs: AttributeSet?) : LinearLayout(con
             list.forEachIndexed { index, item ->
                 var isPresent = false
                 // check if item is present
-                for (i in 0 until spanCount) {
+                for (i in 0 until listView.spanCount) {
                     if (columnItems[i].contains(item)) {
                         isPresent = true
                         break
@@ -83,7 +73,7 @@ class ColumnsListView(context: Context, attrs: AttributeSet?) : LinearLayout(con
                     var min = columnItems[0].size
 
                     // find less full column
-                    for (i in 0 until spanCount) {
+                    for (i in 0 until listView.spanCount) {
                         if (columnItems[i].size < min) {
                             columnIndex = i
                             min = columnItems[i].size
@@ -92,35 +82,37 @@ class ColumnsListView(context: Context, attrs: AttributeSet?) : LinearLayout(con
 
                     // add new item
                     columnItems[columnIndex].add(item)
-                    val holder = onCreateHolder(columns[columnIndex])
+                    val holder = onCreateHolder(column(columnIndex))
                     holder.view.tag = holder
                     onBindHolder(holder, item)
-                    columns[columnIndex].addView(holder.view)
+                    column(columnIndex).addView(holder.view)
                 }
             }
         }
 
         fun removeItem(item: T) {
-            for (i in 0 until spanCount) {
+            for (i in 0 until listView.spanCount) {
                 val position = columnItems[i].indexOf(item)
                 if (position != -1) {
                     columnItems[i].removeAt(position)
-                    columns[i].removeViewAt(position)
+                    column(i).removeViewAt(position)
                     return
                 }
             }
         }
 
         fun updateItem(item: T) {
-            for (i in 0 until spanCount) {
+            for (i in 0 until listView.spanCount) {
                 val position = columnItems[i].indexOf(item)
                 if (position != -1) {
                     columnItems[i].set(position, item)
-                    val holder = columns[i].getChildAt(position).tag as Holder<T>
+                    val holder = column(i).getChildAt(position).tag as Holder<T>
                     onBindHolder(holder, item)
                     return
                 }
             }
         }
+
+        private fun column(index: Int) = listView.getChildAt(index) as LinearLayout
     }
 }
